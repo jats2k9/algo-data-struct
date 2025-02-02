afde2b5f1884b2b61ae04a18da4c0fd6edff06b7
jats2k9 <jats2k9@gmail.com>
2025-02-01 22:37:46 -0500

    SELECT category for SQL

--- src/sql/StrataScratch.sql
+++ src/sql/StrataScratch.sql
@@ -1,8 +1,43 @@
 /*
  https://platform.stratascratch.com/coding/2009-users-with-two-statuses?code_type=1
  */
-
 SELECT user_id
 FROM twitch_sessions
 GROUP BY user_id
 HAVING COUNT(DISTINCT session_type) = 2;
+
+/*
+ https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+ */
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+/*
+ https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+ */
+SELECT name
+FROM Customer
+WHERE referee_id != 2 OR referee_id IS NULL;
+
+/*
+ https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
+ */
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
+
+/*
+ https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+ */
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC
+
+/*
+ https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&envId=top-sql-50
+ */
+ SELECT tweet_id
+ FROM Tweets
+ WHERE CHAR_LENGTH(content) > 15;
