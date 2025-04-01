10c511dc40b130e82d455b4a266b82e8225594a5
jats2k9 <jats2k9@gmail.com>
2025-04-01 19:58:14 -0400

    longest-palindromic-substring

--- src/strings/Strings.java
+++ src/strings/Strings.java
@@ -3,6 +3,8 @@ package strings;
 import java.util.Arrays;
 
 public class Strings {
+    int start = 0;
+    int end = 0;
 
     /**
      * https://leetcode.com/problems/valid-anagram/
@@ -21,5 +23,28 @@ public class Strings {
             count[tChar]--;
         }
         return Arrays.stream(count).filter(value -> value != 0).findFirst().isEmpty();
+    }
+
+    /**
+     * https://leetcode.com/problems/longest-palindromic-substring/
+     */
+    public String longestPalindrome(String s) {
+        if (s.length() == 1) return s;
+        for (int i = 0; i < s.length(); i++) {
+            palindromeSize(s, i, i);
+            palindromeSize(s, i, i + 1);
+        }
+        return s.substring(start + 1, end);
+    }
+
+    private void palindromeSize(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        if (r - l > 0 && r - l > end - start) {
+            start = l;
+            end = r;
+        }
     }
 }
