fadaa4e1d84d239b62fbee17b1b153359c51b7b3
jats2k9 <jats2k9@gmail.com>
2025-04-03 14:37:56 -0400

    best-time-to-buy-and-sell-stock

--- src/two/pointers/TwoPointers.java
+++ src/two/pointers/TwoPointers.java
@@ -178,4 +178,27 @@ public class TwoPointers {
         }
         return new int[0];
     }
+
+    /**
+     * https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+     */
+    public static int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int curr = prices[i];
+            if (buy > curr) {
+                buy = curr;
+            }
+            profit = Math.max(profit, curr - buy);
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{3,2,1}));
+    }
+
 }
